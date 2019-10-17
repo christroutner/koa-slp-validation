491e42423a0b8ce96fe44e3c2051e7b6bb5e89a8
Chris Troutner <chris.troutner@gmail.com>
2019-10-17 13:11:23 -0700

    Making external interface the default for rpc

--- config/env/common.js
+++ config/env/common.js
@@ -7,5 +7,5 @@ module.exports = {
   port: process.env.PORT || 5001,
   rpcUserName: process.env.RPC_USER_NAME ? process.env.RPC_USER_NAME : 'bitcoin',
   rpcPassword: process.env.RPC_PASSWORD ? process.env.RPC_PASSWORD : 'password',
-  rpcUrl: process.env.RPC_URL ? process.env.RPC_URL : 'localhost:8332'
+  rpcUrl: process.env.RPC_URL ? process.env.RPC_URL : '0.0.0.0:8332'
 }
